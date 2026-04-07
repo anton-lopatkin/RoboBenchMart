aacdd7e1c99e24040fa4b44fabbfe236c3640745
Anton Lopatkin <m2205645@edu.misis.ru>
2026-04-07 18:43:42 +0300

    upd: controller api

--- planning/controller.py
+++ planning/controller.py
@@ -1,27 +1,20 @@
 import numpy as np
 import sapien
+from typing import Sequence
 from mani_skill.utils import common
+from mani_skill.examples.motionplanning.base_motionplanner import utils as maniskill_mp_utils
 
-from mani_skill.examples.motionplanning.base_motionplanner.utils import (
-    get_actor_obb,
-    compute_grasp_info_by_obb
-)
-
-from dsynth.planning.motionplanner import FetchMotionPlanningSapienSolver
-from dsynth.planning.utils import (
-    get_fcl_object_name,
-    is_mesh_cylindrical
-)
+from dsynth.planning import motionplanner
+from dsynth.planning import utils as dsynth_mp_utils
 
 
 class Controller:
-    
     FINGER_LENGTH = 0.04
 
     def __init__(self, env, debug=False, vis=False):
         self.env = env.unwrapped
 
-        self.solver = FetchMotionPlanningSapienSolver(
+        self.solver = motionplanner.FetchMotionPlanningSapienSolver(
             env,
             debug=debug,
             vis=vis,
@@ -29,174 +22,245 @@ class Controller:
             visualize_target_grasp_pose=vis,
             print_env_info=False,
             disable_actors_collision=False,
-            verbose=debug
+            verbose=debug,
         )
 
-        self.neutral_ee_pose_wrt_to_base = self.get_base_pose().sp.inv() * self.get_tcp_pose().sp * sapien.Pose(p=[0.1, 0, 0.1])
-        
+        self.neutral_tcp_pose_wrt_to_base = ( 
+            self._base_pose.inv() * self._tcp_pose * sapien.Pose(p=[0.1, 0, 0.1])
+        )
 
     def move_ee_to_neutral_pose(self):
-        neutral_pose = self.get_base_pose().sp * self.neutral_ee_pose_wrt_to_base
-        result = self.solver.static_manipulation(neutral_pose, n_init_qpos=400, disable_lift_joint=False)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
+        """Move end-effector to a neutral pose"""
+        target_pose = self._base_pose * self.neutral_tcp_pose_wrt_to_base
+        return self._run(
+            self.solver.static_manipulation,
+            target_tcp_pose=target_pose,
+            n_init_qpos=400,
+            disable_lift_joint=False,
+        )
 
-    def move_ee_to_product_height(self, item_id):
-        target_product_actor = [item for item in self.env.actors['products'].values() if item.per_scene_id[0].item() == item_id][0]
-        target_product_obb = get_actor_obb(target_product_actor)
-        target_product_center = self.get_obb_center(target_product_obb)
+
+    def move_ee_to_pregrasp_pose(self, item_id: int):
+        """Move end-effector to an approach position near the product.
+
+        Positions the end-effector at the product's height and slightly forward. 
+
+        Args:
+            item_id (int): ID of the product to align with.
+        """
+        product_center = self._product_center(item_id)
+        neutral_pose = self._base_pose * self.neutral_tcp_pose_wrt_to_base
+        target_pose = neutral_pose * sapien.Pose([0, 0, 0.2])
+        target_pose.p[2] = product_center[2]
+        return self._run(
+            self.solver.static_manipulation,
+            target_tcp_pose=target_pose,
+            n_init_qpos=50,
+            disable_lift_joint=False,
+        )
+
+
+    def move_ee_to_grasp_pose(self, item_id: int):
+        """Move end-effector to the grasp pose for an item.
+
+        Prerequisites:
+        - Use move_ee_to_pregrasp_pose first to get into a good starting position.
+
+        Args:
+            item_id (int): ID of the product to grasp.
+        """
+        product_actor = self._product_actor(item_id)
         
-        lift_ee_pos = self.get_tcp_pose().sp.p
-        lift_ee_pos[2] = target_product_center[2]
-        lift_ee_pose = sapien.Pose(p=lift_ee_pos, q=self.get_tcp_pose().sp.q)
-        lift_ee_pose = lift_ee_pose * sapien.Pose(p=[0, 0, 0.3])
-
-        result = self.solver.static_manipulation(lift_ee_pose, n_init_qpos=50, disable_lift_joint=False)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def lift_ee(self, delta):
-        lift_pose = self.get_tcp_pose().sp * sapien.Pose([delta, 0., 0.])
-        result = self.solver.static_manipulation(lift_pose, n_init_qpos=200, disable_lift_joint=False)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def drive_to_shelf(self):        
-        actor_shelf_name = self.env.active_shelves[0][0]
-        shelf_pose = self.env.actors["fixtures"]["shelves"][actor_shelf_name].pose.sp
-        origin = shelf_pose.p - 1.4 * self.env.directions_to_shelf[0]
-
-        result = self.solver.drive_base(origin)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def drive_to_product(self, item_id):
-        target_product_actor = [item for item in self.env.actors['products'].values() if item.per_scene_id[0].item() == item_id][0]
-        product_pose = target_product_actor.pose.sp 
-        direction_to_product = product_pose.to_transformation_matrix()[:3, 1]
-        target_pose = product_pose.p - 1.2 * direction_to_product
-
-        result = self.solver.drive_base(target_pose)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def move_base_forward(self, delta):
-        result = self.solver.move_forward_delta(delta)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def rotate_base(self, delta):
-        result = self.solver.rotate_z_delta(delta)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def align_to_product(self, item_id):
-        target_product_actor = [item for item in self.env.actors['products'].values() if item.per_scene_id[0].item() == item_id][0]
-        target_product_obb = get_actor_obb(target_product_actor)
-        target_product_center = self.get_obb_center(target_product_obb)
-
-        view_to_target = target_product_center - self.get_base_pose().sp.p
-        view_to_target[2] = 0.
-
-        result = self.solver.rotate_base_z(view_to_target)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def move_base_towards_product(self, item_id):
-        target_product_actor = [item for item in self.env.actors['products'].values() if item.per_scene_id[0].item() == item_id][0]
-        target_product_obb = get_actor_obb(target_product_actor)
-        target_product_center = self.get_obb_center(target_product_obb)
-
-        pre_grasp_base_translation = target_product_center - self.get_tcp_center()
-        pre_grasp_base_translation[2] = 0.
-
-        # move base to position 0.15m in fornt of the target object
-        base_target_pos = self.get_base_pose().sp.p + \
-            (1 - 0.2 / np.linalg.norm(pre_grasp_base_translation)) * pre_grasp_base_translation
-        
-        result = self.solver.drive_base(base_target_pos)
-        if result == -1:
-            return result
-        self.solver.planner.update_from_simulation()
-
-    def grasp_product(self, item_id):
-        target_product_actor = [item for item in self.env.actors['products'].values() if item.per_scene_id[0].item() == item_id][0]
-        target_product_obb = get_actor_obb(target_product_actor)
-        target_product_center = self.get_obb_center(target_product_obb)
-
-        if is_mesh_cylindrical(target_product_actor):
-            grasp_approaching = self.env.directions_to_shelf[0].copy()
-            grasp_approaching[2] = 0.
-            grasp_approaching = common.np_normalize_vector(grasp_approaching)
-
-            grasp_closing = np.cross(grasp_approaching, [0., 0., 1.])
-            grasp_center = target_product_center
-
-        else: 
-            grasp_info = compute_grasp_info_by_obb(
-                target_product_obb,
-                approaching=self.get_tcp_matrix()[:3, 2],
-                target_closing=self.get_tcp_matrix()[:3, 1],
-                depth=self.FINGER_LENGTH
-            )
-            grasp_closing, grasp_center, grasp_approaching = grasp_info["closing"], grasp_info["center"], grasp_info["approaching"]
-
-        grasp_pose = self.env.agent.build_grasp_pose(grasp_approaching, grasp_closing, grasp_center)
+        grasp_pose = self._compute_grasp_pose(product_actor)
 
         self.solver.planner.planning_world.get_allowed_collision_matrix().set_default_entry(
-            get_fcl_object_name(target_product_actor), True
+            dsynth_mp_utils.get_fcl_object_name(product_actor), True
         )
-        result = self.solver.static_manipulation(grasp_pose, n_init_qpos=400, disable_lift_joint=False)
-        if result == -1:
-            return result
-        
-        result = self.solver.close_gripper()
-        if result == -1:
-            return result
-        
-        kwargs = {"name": get_fcl_object_name(target_product_actor), "art_name": 'scene-0_ds_fetch_basket_1', "link_id": self.solver.planner.move_group_link_id}
-        self.solver.planner.planning_world.attach_object(**kwargs)
-        self.solver.planner.update_from_simulation()
 
-    def drop_to_basket(self):
+        return self._run(
+            self.solver.static_manipulation,
+            target_tcp_pose=grasp_pose,
+            n_init_qpos=400,
+            disable_lift_joint=False,
+        )
+
+    def move_ee_to_drop_pose(self):
+        """Move end-effector to a drop pose above the robot's basket"""
         goal_center = self.env.calc_target_pose().sp.p
-        goal_approaching = np.array([0, 0., -1.])
-        goal_closing = - self.get_base_pose().sp.to_transformation_matrix()[:3, 1]
+        goal_approaching = np.array([0, 0.0, -1.0])
+        goal_closing = -self._base_pose.to_transformation_matrix()[:3, 1]
 
-        goal_pose = self.env.agent.build_grasp_pose(goal_approaching, goal_closing, goal_center)
-        goal_pose = goal_pose * sapien.Pose(p=[-0.03, 0., -0.35])
+        goal_pose = self.env.agent.build_grasp_pose(
+            goal_approaching, goal_closing, goal_center
+        )
+        goal_pose = goal_pose * sapien.Pose(p=[-0.03, 0.0, -0.35])
 
-        result = self.solver.static_manipulation(goal_pose, n_init_qpos=100, disable_lift_joint=False)
-        if result == -1:
-            return result
-        
-        result = self.solver.open_gripper()
+        return self._run(
+            self.solver.static_manipulation,
+            target_tcp_pose=goal_pose,
+            n_init_qpos=100,
+            disable_lift_joint=False,
+        )
+
+    def move_ee_by(self, delta: Sequence[float]):
+        """Move end-effector by a relative offset.
+
+        Args:
+            delta (Sequence[float]): Relative offset [dx, dy, dz] in meters in 
+                end-effector frame:
+                - X: up/down
+                - Y: left/right
+                - Z: forward/backward
+        """
+        target_pose = self._tcp_pose * sapien.Pose(delta)
+        return self._run(
+            self.solver.static_manipulation,
+            target_tcp_pose=target_pose,
+            n_init_qpos=200,
+            disable_lift_joint=False,
+        )
+
+    def grasp(self, item_id: int):
+        """Close gripper to pick up a product.
+
+         Prerequisites:
+        - End-effector must already be positioned at the grasp pose.
+
+        Args:
+            item_id (int): ID of the product to grasp.
+        """
+        product_actor = self._product_actor(item_id)
+        result = self._run(self.solver.close_gripper)
+        self.solver.planner.planning_world.attach_object(
+            name=dsynth_mp_utils.get_fcl_object_name(product_actor),
+            art_name="scene-0_ds_fetch_basket_1",
+            link_id=self.solver.planner.move_group_link_id,
+        )
+        self.solver.planner.update_from_simulation()
+        return result
+
+    def release(self):
+        """Open gripper to release the currently held item."""
+        return self._run(self.solver.open_gripper)
+
+    def drive_to_shelf(self, distance: float):
+        """Drive the robot base to approach the center of the active shelf.
+
+        Args:
+            distance (float): Distance in meters to stop from the shelf
+        """
+        shelf_name = self.env.active_shelves[0][0]
+        shelf_pose = self.env.actors["fixtures"]["shelves"][shelf_name].pose.sp
+        target = shelf_pose.p - distance * self.env.directions_to_shelf[0]
+        return self._run(self.solver.drive_base, target_pos=target)
+
+    def drive_to_product(self, item_id: int, distance: float):
+        """Drive the robot base toward a specific product.
+
+        Args:
+            item_id (int): ID of the product to drive toward.
+            distance (float): Distance in meters to stop from the product.
+        """
+        product_actor = self._product_actor(item_id)
+        product_pose = product_actor.pose.sp
+        direction = product_pose.to_transformation_matrix()[:3, 1]
+        target_pos = product_pose.p - distance * direction
+        return self._run(self.solver.drive_base, target_pos=target_pos)
+
+    def move_base_forward(self, delta: float):
+        """Move the robot base forward or backward by a delta distance.
+
+        Args:
+            delta (float): Distance in meters. Positive moves forward, negative moves
+                backward.
+        """
+        return self._run(self.solver.move_forward_delta, delta=delta)
+
+    def rotate_base(self, delta: float):
+        """Rotate the robot base around its Z axis.
+
+        Args:
+            delta (float): Rotation angle in radians. Positive is counterclockwise,
+                negative is clockwise.
+        """
+        return self._run(self.solver.rotate_z_delta, delta=delta)
+
+    def align_to_product(self, item_id: int):
+        """Rotate the robot base to face a product.
+
+        Args:
+            item_id (int): ID of the product to align toward.
+        """
+        direction = self._product_center(item_id) - self._base_pose.p
+        direction[2] = 0.0
+        return self._run(self.solver.rotate_base_z, new_direction=direction)
+
+
+    def _run(self, fn, **kwargs):
+        result = fn(**kwargs)
         if result == -1:
             return result
         self.solver.planner.update_from_simulation()
+        return result
 
+    @property
+    def _base_pose(self):
+        return self.env.agent.base_link.pose.sp
 
-    def get_obb_center(self, obb):
+    @property
+    def _tcp_pose(self):
+        return self.env.agent.tcp.pose.sp
+
+    @property
+    def _tcp_matrix(self):
+        return self.env.agent.tcp.pose.to_transformation_matrix()[0].cpu().numpy()
+
+    @property
+    def _tcp_center(self):
+        return self._tcp_matrix[:3, 3]
+
+    def _obb_center(self, obb):
         T = np.array(obb.primitive.transform)
         return T[:3, 3]
 
-    def get_base_pose(self):
-        return self.env.agent.base_link.pose
-        
-    def get_tcp_pose(self):
-        return self.env.agent.tcp.pose
+    def _product_actor(self, item_id):
+        return [
+            item
+            for item in self.env.actors["products"].values()
+            if item.per_scene_id[0].item() == item_id
+        ][0]
 
-    def get_tcp_matrix(self):
-        tcp_pose = self.get_tcp_pose()
-        return tcp_pose.to_transformation_matrix()[0].cpu().numpy()
-        
-    def get_tcp_center(self):
-        return self.get_tcp_matrix()[:3, 3]
+    def _product_center(self, item_id):
+        product_actor = self._product_actor(item_id)
+        product_obb = maniskill_mp_utils.get_actor_obb(product_actor)
+        product_center = self._obb_center(product_obb)
+        return product_center
+    
+    def _compute_grasp_pose(self, product_actor):
+        product_obb = maniskill_mp_utils.get_actor_obb(product_actor)
+        product_center = self._obb_center(product_obb)
+
+        if dsynth_mp_utils.is_mesh_cylindrical(product_actor):
+            grasp_approaching = self.env.directions_to_shelf[0].copy()
+            grasp_approaching[2] = 0.0
+            grasp_approaching = common.np_normalize_vector(grasp_approaching)
+            grasp_closing = np.cross(grasp_approaching, [0.0, 0.0, 1.0])
+            grasp_center = product_center
+        else:
+            grasp_info = maniskill_mp_utils.compute_grasp_info_by_obb(
+                product_obb,
+                approaching=self._tcp_matrix[:3, 2],
+                target_closing=self._tcp_matrix[:3, 1],
+                depth=self.FINGER_LENGTH,
+            )
+            grasp_closing, grasp_center, grasp_approaching = (
+                grasp_info["closing"],
+                grasp_info["center"],
+                grasp_info["approaching"],
+            )
+
+        grasp_pose = self.env.agent.build_grasp_pose(
+            grasp_approaching, grasp_closing, grasp_center
+        )
+
+        return grasp_pose
